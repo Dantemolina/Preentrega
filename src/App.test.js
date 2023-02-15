ec0dd8c9dd6a7e74726282a68e6fc2a78808092e
dantemolina <dantemolina00@gmail.com>
2023-02-14 21:55:04 -0300

    primera version

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import NavBar from './Componentes/NavBar';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<NavBar />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
